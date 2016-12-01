43033591f1b880d7bfbdcd648e5d253d3cc4b6b6
junpei yamaguchi <yamaguchi.cocytos@gmail.com>
2016-12-02 08:20:16 +0900

    3回目

--- src/title.js
+++ src/title.js
@@ -16,7 +16,8 @@ var MyLayer = cc.Layer.extend({
         }
 
 
-        var Title_png = cc.Sprite.create(res.Title_png);
+        var Title_png = cc.Sprite.create(res.howtoplay_png);
+
         Title_png.setPosition(size.width / 2, size.height / 2);
         this.addChild(Title_png);
         //add code
